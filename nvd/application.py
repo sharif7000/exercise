6f41bbe232979f6db5496a0d6752aea5dfb4af68
sharif7000 <69607928+sharif7000@users.noreply.github.com>
2023-03-16 22:43:04 +0530

    Code refactor with nvd end date logic correction (#2)
    * Moved variables to constants
    
    * Updated nvd code with end date logic correction

--- nvd/application.py
+++ nvd/application.py
@@ -4,75 +4,61 @@ import pandas as pd
 import datetime
 import variables
 
-date = variables.date
-cpeName = variables.cpeName
-#date = '2022-01-01'
-tmpStartDate = dateutil.parser.isoparse(date)
-print(tmpStartDate)
+# Get start date
+tmpStartDate = dateutil.parser.isoparse(variables.start_date)
+# Initialize end date to +120 days from start date
 tmpEndDate = tmpStartDate + datetime.timedelta(days=120)
-#cpeName = "cpe:2.3:o:redhat:enterprise_linux:8.0"
 
-print("Start of process")
+# If given end date is less tha 120 days, correct the end date by assigning the provided
+if tmpEndDate > dateutil.parser.isoparse(variables.end_date):
+    tmpEndDate = variables.end_date
+
+
+print("Fetching data in bacthes of 120 days")
 data_set = []
-while tmpStartDate < datetime.datetime.now():
-    #print("Fetching data from " , tmpStartDate ," To " , tmpEndDate)
+while tmpStartDate < dateutil.parser.isoparse(variables.end_date):
+
     fomrattedStartDate = tmpStartDate.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
     fomrattedEndDate = tmpEndDate.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-    ## CALL API
-    base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
-    query_params = {"cpeName": cpeName, "pubStartDate": fomrattedStartDate, "pubEndDate": fomrattedEndDate}
-    response = requests.get(base_url, params=query_params).json()
+    print("Fetching data from ", fomrattedStartDate, " To ", fomrattedEndDate)
+    query_params = {"cpeName": variables.cpeName,
+                    "pubStartDate": fomrattedStartDate, "pubEndDate": fomrattedEndDate}
+    response = requests.get(variables.BASE_URL, params=query_params).json()
 
     results = response['vulnerabilities']
-
-   
+    print("Received data with size ", len(results))
     for result in results:
-            cve_id = result['cve']['id']
-            cisa_vuln_name = ''
-            try:
-                cisa_vuln_name = result['cve']['cisaVulnerabilityName']
-            except KeyError:
-                print()
-            is_exploited = False
-            for reference in result['cve']['references']:
-                if 'tags' in reference and 'Exploit' in reference['tags']:
-                    is_exploited = True
-                    break
-            cvss_base_score = None
-            cvss_metric_v31 = result['cve']['metrics'].get('cvssMetricV31', [])
-            #print(cvss_metric_v31)
-            if cvss_metric_v31:
-                cvss_base_score = cvss_metric_v31[0]['cvssData']['baseScore']
-            else:
-                cvss_metric_v2 = result['cve']['metrics'].get('cvssMetricV2', [])
-                if cvss_metric_v2:
-                    cvss_base_score = cvss_metric_v2[0]['cvssData']['baseScore']
-            vuln_description = result['cve']['descriptions'][0]['value'][:400]
+        cve_id = result['cve']['id']
+        cisa_vuln_name = ''
+        try:
+            cisa_vuln_name = result['cve']['cisaVulnerabilityName']
+        except KeyError:
+            pass
+        is_exploited = False
+        for reference in result['cve']['references']:
+            if 'tags' in reference and 'Exploit' in reference['tags']:
+                is_exploited = True
+                break
+        cvss_base_score = None
+        cvss_metric_v31 = result['cve']['metrics'].get('cvssMetricV31', [])
+        if cvss_metric_v31:
+            cvss_base_score = cvss_metric_v31[0]['cvssData']['baseScore']
+        else:
+            cvss_metric_v2 = result['cve']['metrics'].get('cvssMetricV2', [])
+            if cvss_metric_v2:
+                cvss_base_score = cvss_metric_v2[0]['cvssData']['baseScore']
+        vuln_description = result['cve']['descriptions'][0]['value'][:400]
 
-            
-            tmpData = {"CVE ID": cve_id, "CISA Vulnerability Name": cisa_vuln_name, "Exploited": is_exploited, "CVSS Base Score": cvss_base_score, "Vulnerability Description": vuln_description}
+        tmpData = {"CVE ID": cve_id, "CISA Vulnerability Name": cisa_vuln_name, "Exploited": is_exploited,
+                   "CVSS Base Score": cvss_base_score, "Vulnerability Description": vuln_description}
+        data_set.append(tmpData)
 
-            #print(tmpData)
-            data_set.append(tmpData)
-
-    
-            #Print extracted information
-            # print(f"CVE ID: {cve_id}")
-            # print(f"CISA Vulnerability Name: {cisa_vuln_name}")
-            # print(f"Exploited: {is_exploited}")
-            # print(f"CVSS Base Score: {cvss_base_score}")
-            # print(f"Vulnerability Description: {vuln_description}")
-    
-    
-
-
-
-    #print(data_set)        
     tmpStartDate = tmpEndDate
     tmpEndDate = tmpStartDate + datetime.timedelta(days=120)
+    if tmpEndDate > dateutil.parser.isoparse(variables.end_date):
+        tmpEndDate = dateutil.parser.isoparse(variables.end_date)
 
 
-#print(data_set)
 # Add to CSV file
 df = pd.json_normalize(data_set)
 df.to_csv("CVE_data_new.csv", index=False)
